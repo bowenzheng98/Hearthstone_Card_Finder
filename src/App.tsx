d8b345380e0e1e70951d9b2f6a975344f1c9a6ff
Bowen Zheng <bowenzheng998@gmail.com>
2018-09-10 23:12:57 +1200

    Implemented fetching a card

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,32 @@ class App extends React.Component<{},IHearthstoneCard> {
   constructor(props:any){
     super(props);
     this.state = {
-      image: "https://d1u5p3l4wpay3k.cloudfront.net/hearthstone_gamepedia/c/c8/Stonetusk_Boar%2876%29.png?version=feaca8125122935600ae22c550e464f5c",
+      image: "http://media.services.zam.com/v1/media/byName/hs/cards/enus/CS2_171.png",
       name: "",
       result: ""
-    }
+    };  
+    this.setName = this.setName.bind(this);
+    this.getCard = this.getCard.bind(this);
   }
 
-  public getCard(cardName:any){
-    fetch('https://omgvamp-hearthstone-v1.p.mashape.com/cards/' + cardName)
-    .then(results => {
-      return results.json;
+  public getCard = () => {
+    fetch('https://omgvamp-hearthstone-v1.p.mashape.com/cards/' + this.state.name, {
+      headers: {
+        'Accept' : 'application/json',
+        'X-Mashape-Key' : 'umhsTza6z7mshV6ax0uG7zY5uY9dp180Mlhjsnh1ZK83NPtgzA'
+      }
+    }).then((result) => {
+      return result.json();
+    }).then((data) => {
+      // tslint:disable-next-line:no-console
+      console.log(data[0].img);
+      const imageData = data[0].img;
+      this.setState({image: imageData});
     })
+  }
+
+  public setName(e: React.ChangeEvent<HTMLInputElement>){
+    this.setState({name: e.target.value})
   }
 
   public render() {
@@ -34,9 +49,9 @@ class App extends React.Component<{},IHearthstoneCard> {
           </header>
         </div>
         <div>
-          <Card className="Main-card">
-            <Input className="Input" placeholder="Card name"/>
-            <Button className="Search-button" variant="contained">Search</Button>
+          <Card className="Main-card">  
+            <Input className="Input" placeholder="Card name" value={this.state.name} onChange={this.setName}/>
+            <Button className="Search-button" variant="contained" onClick={this.getCard} >Search</Button>
           </Card>
         </div>
         <div>
