0359eabedd1ca2e2752a84515bd82c870a3d8f0a
Bowen Zheng <bowenzheng998@gmail.com>
2018-09-10 17:39:44 +1200

    Implemented base interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,49 @@
+import { Button, Card, Input } from '@material-ui/core';
 import * as React from 'react';
 import './App.css';
 
-class App extends React.Component {
+interface IHearthstoneCard{
+  image: string;
+  name: string;
+  result: any;
+}
+
+class App extends React.Component<{},IHearthstoneCard> {
+  constructor(props:any){
+    super(props);
+    this.state = {
+      image: "",
+      name: "",
+      result: ""
+    }
+  }
+
+  public getCard(cardName:string){
+    fetch('https://omgvamp-hearthstone-v1.p.mashape.com/cards/' + cardName)
+    .then(results => {
+      return results.json;
+    })
+  }
+
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <body>
+        <div className="App">
+          <header className="App-header">
+            <div className="Header-text">HearthStone Card Finder</div>
+          </header>
+        </div>
+        <div>
+          <Card className="Main-card">
+            <Input className="Input" placeholder="Card name"/>
+            <Button className="Search-button" variant="contained">Search</Button>
+          </Card>
+        </div>
+        <div>
+          <Card/>
+        </div>
+      </body>
     );
   }
 }
-
 export default App;
